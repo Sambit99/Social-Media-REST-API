6e59cc1c40add9e02c6638720b5510257d3bdef5
Sambit99 <sambitkumar75@gmail.com>
2025-01-17 12:22:52 +0530

    feat: Add Comment model to manage user comments on posts

---
+++ server/src/model/comment.model.ts
@@ -0,0 +1,20 @@
+import mongoose, { Document, Model, Schema } from 'mongoose';
+
+export interface IComment extends Document {
+  post: Schema.Types.ObjectId;
+  commentedBy: Schema.Types.ObjectId;
+  content: string;
+  likesCount: number;
+}
+
+const commentModel: Schema<IComment> = new mongoose.Schema(
+  {
+    post: { type: Schema.Types.ObjectId, ref: 'Post' },
+    commentedBy: { type: Schema.Types.ObjectId, ref: 'User' },
+    content: { type: String, required: true },
+    likesCount: { type: Number, default: 0 }
+  },
+  { timestamps: true }
+);
+
+export const Comment: Model<IComment> = mongoose.model<IComment>('Comment', commentModel);
