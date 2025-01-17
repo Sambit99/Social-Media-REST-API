3ac77cce5084681829fe93bfb6b36b88f8341a1d
Sambit99 <sambitkumar75@gmail.com>
2025-01-17 12:26:12 +0530

    feat: Add Like model to manage user likes on posts

---
+++ server/src/model/like.model.ts
@@ -0,0 +1,16 @@
+import mongoose, { Document, Model, Schema } from 'mongoose';
+
+export interface ILike extends Document {
+  post: Schema.Types.ObjectId;
+  likedBy: Schema.Types.ObjectId;
+}
+
+const likeModel: Schema<ILike> = new mongoose.Schema(
+  {
+    post: { type: Schema.Types.ObjectId, ref: 'Post' },
+    likedBy: { type: Schema.Types.ObjectId, ref: 'User' }
+  },
+  { timestamps: true }
+);
+
+export const Like: Model<ILike> = mongoose.model<ILike>('Like', likeModel);
