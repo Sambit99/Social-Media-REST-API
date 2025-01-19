7eddcb1c58ed98a6c1a5bb9f777047a5ed041455
Sambit99 <sambitkumar75@gmail.com>
2025-01-19 21:33:30 +0530

    feat: Enhance like model to include comment reference and update post likes count on save

--- server/src/model/like.model.ts
+++ server/src/model/like.model.ts
@@ -1,16 +1,27 @@
 import mongoose, { Document, Model, Schema } from 'mongoose';
+import { Post } from './post.model';
 
 export interface ILike extends Document {
   post: Schema.Types.ObjectId;
+  comment: Schema.Types.ObjectId;
   likedBy: Schema.Types.ObjectId;
 }
 
 const likeSchema: Schema<ILike> = new mongoose.Schema(
   {
     post: { type: Schema.Types.ObjectId, ref: 'Post' },
+    comment: { type: Schema.Types.ObjectId, ref: 'Comment' },
     likedBy: { type: Schema.Types.ObjectId, ref: 'User' }
   },
   { timestamps: true }
 );
+
+likeSchema.post('save', async function () {
+  if (this.post) {
+    const totalLikesForThePost: number = await Like.countDocuments({ post: this.post });
+
+    await Post.findByIdAndUpdate(this.post, { likesCount: totalLikesForThePost });
+  }
+});
 
 export const Like: Model<ILike> = mongoose.model<ILike>('Like', likeSchema);
