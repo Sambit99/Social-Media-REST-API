9563c73e3d9c40b3935cceba088de7b23797e138
Sambit99 <sambitkumar75@gmail.com>
2025-01-22 12:52:07 +0530

    feat: Add Notification model to handle user notifications

---
+++ server/src/model/notification.model.ts
@@ -0,0 +1,30 @@
+import mongoose, { Document, Model, Schema } from 'mongoose';
+
+enum NOTIFICATION_TYPES {
+  POST = 'post',
+  COMMENT = 'comment',
+  LIKE = 'like'
+}
+
+interface INotification extends Document {
+  from: Schema.Types.ObjectId;
+  to: Schema.Types.ObjectId;
+  type: string;
+  isRead: boolean;
+  content: string;
+  resourceId: string;
+}
+
+const notificationSchema: Schema<INotification> = new mongoose.Schema(
+  {
+    from: { type: Schema.Types.ObjectId, ref: 'User' },
+    to: { type: Schema.Types.ObjectId, ref: 'User' },
+    type: { type: String, enum: NOTIFICATION_TYPES },
+    isRead: { type: Boolean, default: false },
+    content: { type: String, required: true },
+    resourceId: { type: String, required: true }
+  },
+  { timestamps: true }
+);
+
+export const Notification: Model<INotification> = mongoose.model<INotification>('Notification', notificationSchema);
