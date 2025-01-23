1aa2e65518156aae4345525e03fc9fb9bb55f228
Sambit99 <sambitkumar75@gmail.com>
2025-01-23 20:25:35 +0530

    feat: Add routes to get and delete a post by ID in admin panel

--- server/src/routes/admin.routes.ts
+++ server/src/routes/admin.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { isLoggedIn, restrictTo } from '../middleware/auth.middleware';
 import { deleteUserById, getAllUsers, getUserById } from '../controller/user.controller';
+import { deletePostById, getPostById } from '../controller/post.controller';
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use(restrictTo('admin'));
 
 router.route('/users').get(getAllUsers);
 router.route('/user/:userId').get(getUserById).delete(deleteUserById);
+router.route('/post/:postId').get(getPostById).delete(deletePostById);
 
 export default router;
