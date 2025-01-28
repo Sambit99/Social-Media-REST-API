204531d0c569423b635184787bd2e7272629dcc0
Sambit99 <sambitkumar75@gmail.com>
2025-01-28 21:38:52 +0530

    feat: Add time constants for day and hour in seconds

--- server/src/constant/application.ts
+++ server/src/constant/application.ts
@@ -2,3 +2,9 @@ export enum ApplicationEnvironment {
   PRODUCTION = 'production',
   DEVELOPMENT = 'development'
 }
+
+export const TimeInSeconds = {
+  DAY_IN_SECONDS: 24 * 60 * 60,
+  HOUR_IN_SECONDS: 60 * 60,
+  X_DAYS_IN_SECONDS: (x: number) => x * 24 * 60 * 60
+};
