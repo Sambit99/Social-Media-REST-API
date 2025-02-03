e2c338d3f6d7927f8fc87997f01e454806052928
Sambit99 <sambitkumar75@gmail.com>
2025-02-03 14:47:06 +0530

    feat: Add route for API self endpoint

--- server/src/routes/api.routes.ts
+++ server/src/routes/api.routes.ts
@@ -5,6 +5,7 @@ import rateLimit from '../middleware/rateLimit';
 const router = Router();
 
 router.use(rateLimit);
+router.route('/').get(apiController.self);
 router.route('/self').get(apiController.self);
 router.route('/health').get(apiController.health);
 
