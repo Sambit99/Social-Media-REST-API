4ffce37384e001cb9f95a8cf5d328687a9efaac0
Sambit99 <sambitkumar75@gmail.com>
2025-02-04 14:02:18 +0530

    test: Add unit tests for API routes using Axios instance

---
+++ server/src/tests/api.test.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import api from '../util/AxiosInstance';
+
+const HttpResponseObject = {
+  success: expect.any(Boolean),
+  status: expect.any(Number),
+  request: {
+    ip: expect.any(String),
+    method: expect.any(String),
+    url: expect.any(String)
+  },
+  message: expect.any(String),
+  data: {}
+};
+
+describe('Api Routes', () => {
+  it('should get the self route', async () => {
+    const response = await api.get('/self');
+
+    expect(response.status).toBe(200);
+    expect(response.data).toMatchObject(HttpResponseObject);
+    expect(response.data).toMatchObject(HttpResponseObject);
+  });
+
+  it('should get the health route', async () => {
+    const response = await api.get('/health');
+
+    expect(response.status).toBe(200);
+    expect(response.data).toMatchObject(HttpResponseObject);
+    expect(response.data).toHaveProperty('success', true);
+    expect(response.data).toHaveProperty('message', 'Success: The request was processed successfully.');
+  });
+});
