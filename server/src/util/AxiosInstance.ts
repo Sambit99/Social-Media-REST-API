44050dc6c17068f594d60a68cace1b29375ff7e9
Sambit99 <sambitkumar75@gmail.com>
2025-02-04 14:12:45 +0530

    feat: Enable withCredentials in Axios instance for cookie handling

--- server/src/util/AxiosInstance.ts
+++ server/src/util/AxiosInstance.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'http://localhost:3000/api/v1',
+  withCredentials: true, // Note: Required to send and receive cookies
   headers: {
     'Content-Type': 'application/json'
   }
