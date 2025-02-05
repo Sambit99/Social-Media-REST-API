1d6d24a79b0178ea7ac26939f8659f6eb5602ce5
Sambit99 <sambitkumar75@gmail.com>
2025-02-05 13:11:05 +0530

    feat: Add Jest setup file for environment configuration in tests

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -2,5 +2,6 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'js'],
-  testMatch: ['**/tests/**/*.test.ts']
+  testMatch: ['**/tests/**/*.test.ts'],
+  setupFiles: ['<rootDir>/jest.setup.ts']
 };
